4e4c6571de9b7395b6d1f3aac62ae0280480b2c2
ThatColossalWreck <43423116+ThatColossalWreck@users.noreply.github.com>
2018-10-23 23:07:41 -0400

    Add files via upload

---
+++ OO Project Git/WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class CadetWatch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CadetView = new System.Windows.Forms.DataGridView();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.refreshBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CadetView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CadetView
+            // 
+            this.CadetView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CadetView.Location = new System.Drawing.Point(12, 90);
+            this.CadetView.Name = "CadetView";
+            this.CadetView.Size = new System.Drawing.Size(994, 514);
+            this.CadetView.TabIndex = 0;
+            this.CadetView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.CadetView_CellContentClick);
+            this.CadetView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.CadetView_CellDoubleClick);
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(93, 46);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(280, 20);
+            this.searchBox.TabIndex = 1;
+            this.searchBox.TextChanged += new System.EventHandler(this.searchBox_TextChanged);
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(12, 44);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 23);
+            this.searchBtn.TabIndex = 2;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // refreshBtn
+            // 
+            this.refreshBtn.Location = new System.Drawing.Point(607, 43);
+            this.refreshBtn.Name = "refreshBtn";
+            this.refreshBtn.Size = new System.Drawing.Size(75, 23);
+            this.refreshBtn.TabIndex = 3;
+            this.refreshBtn.Text = "Refresh";
+            this.refreshBtn.UseVisualStyleBackColor = true;
+            this.refreshBtn.Click += new System.EventHandler(this.refreshBtn_Click);
+            // 
+            // CadetWatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 616);
+            this.Controls.Add(this.refreshBtn);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.CadetView);
+            this.Name = "CadetWatch";
+            this.Text = "CadetWatch";
+            this.Load += new System.EventHandler(this.CadetWatch_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CadetView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView CadetView;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Button refreshBtn;
+    }
+}
+
---
+++ OO Project Git/WindowsFormsApp2/WindowsFormsApp2/Form2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        //A constructor that fills in a limited amount of fields in the form, for now
+        public Form2(DataRow[] rows, int i)
+        {
+            InitializeComponent();
+            nameBox.Text = rows[i]["Name"].ToString();
+            IDBox.Text = rows[i]["Eagle ID"].ToString();
+            DoBBox.Text = rows[i]["Date of Birth"].ToString();
+            missionBox.Text = rows[i]["Mission Set Projection"].ToString();
+            msBox.Text = rows[i]["MS Level"].ToString();
+            String path = rows[i]["Photo"].ToString();
+            try
+            {
+                studentImage.Image = Image.FromFile(@path);
+            }catch(System.IO.FileNotFoundException e)
+            {
+                studentImage.Image = Image.FromFile(@"E:\OO Project\Project\Pictures\errorImageResize.png");
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
